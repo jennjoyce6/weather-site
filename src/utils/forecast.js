8f75dd71f072fb35e4948c2198a94bd6511fbc38
Jennifer <jennjoyce06@gmail.com>
2022-03-16 17:34:37 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request=require('request')
+
+const forecast=(latitude,longitude,callback)=>{
+    const url='http://api.weatherstack.com/current?access_key=d53e9590df9986c44188e269111f6fcd&query='+latitude+','+longitude
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback('Unable to connect to location services',undefined)
+        } else if(body.error){
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, 'Todays temperature at '+body.location.name+ ' is '+body.current.temperature+'. The temperature feels like '+body.current.feelslike+'. The weather is '+body.current.weather_descriptions+'. There is a '+body.current.precip+'% chance of rain')
+        }
+    })
+}
+
+module.exports=forecast
